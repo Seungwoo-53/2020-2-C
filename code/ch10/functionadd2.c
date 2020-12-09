603e30072362c0dc4a82ee0595796a06265c703b
Seungwoo-54 <tmddn0849@gmail.com>
2020-12-10 01:38:11 +0900

    upload

---
+++ code/ch10/functionadd2.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 3, b = 5;
+	int add2(int a, int b);
+
+	int sum = add2(a, b);
+	printf("합: %d\n", sum);
+	return 0;
+}
+	int add2(int a, int b)
+	{
+		int sum = a + b;
+		return (sum);
+	}
+	int findin2(int x, int y)
+	{
+		int min = x < y ? x : y;
+		return (min);
+	}
+	
+
